990e7ae260901168e7a8d3022144fcfe4a91ab55
Solaris <91217295+RohanVolety@users.noreply.github.com>
2025-02-15 11:57:44 +0530

    Update JwtAuthenticationFilter.java

--- url-shortner-sb/src/main/java/com/url/shortner/security/jwt/JwtAuthenticationFilter.java
+++ url-shortner-sb/src/main/java/com/url/shortner/security/jwt/JwtAuthenticationFilter.java
@@ -16,41 +16,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//    @Autowired
-//    private JwtUtils jwtTokenProvider;
-//
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        try{
-//            //Get JWT From Header
-//            String jwt=jwtTokenProvider.getJwtFromHeader(request);
-//
-//            if(jwt!=null && jwtTokenProvider.validateToken(jwt)){
-//                String username=jwtTokenProvider.getUserNameFromJwtToken(jwt);
-//                UserDetails userDetails=userDetailsService.loadUserByUsername(username);
-//                if(userDetails!=null ){
-//                    UsernamePasswordAuthenticationToken authentication=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-//                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                    SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//                }
-//            }
-//            //Validate Token
-//            //If Valid Get User Details
-//            //Get user name --> Load user --> Set the auth Context
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        filterChain.doFilter(request,response);
-//    }
-//}
-
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
